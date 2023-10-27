217c2879f487166e579b1846cb95469b77f32c51
Brian Bell <bbell.semm@gmail.com>
2023-10-27 18:46:54 -0400

    Add get_page_summary function

--- Back-end/app.py
+++ Back-end/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, jsonify
 import openai
 import os
 import logging
+import requests
+from bs4 import BeautifulSoup
 
 # Set up OpenAI key
 openai.api_key = os.environ.get("OPENAI_API_KEY")
@@ -9,24 +11,39 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 app = Flask(__name__)
 
 def get_page_summary(url):
-    # TODO: Extract text content from the URL using a library like BeautifulSoup or similar.
-    page_content = ...  # This should contain the main text content from the URL.
+    try:
+        # Get the content of the webpage
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for HTTP errors
 
-    response = openai.Completion.create(
-        engine="gpt-3.5-turbo",
-        prompt=f"Summarize the following content from the website {url}: {page_content}",
-        max_tokens=150
-    )
+        soup = BeautifulSoup(response.content, 'html.parser')
 
-    return response.choices[0].text.strip()
+        # Extract text content from the webpage
+        for script in soup(['script', 'style']):
+            script.extract()
+        page_content = " ".join(soup.stripped_strings)
 
+    except requests.RequestException as e:
+        return f"Error fetching content from the URL: {e}"
+
+    try:
+        response = openai.Completion.create(
+            engine="gpt-3.5-turbo",
+            prompt=f"Summarize the following content from the website {url}: {page_content}",
+            max_tokens=150
+        )
+        return response.choices[0].text.strip()
+
+    except openai.error.OpenAIError as e:
+        return f"Error getting summary from OpenAI: {e}"
 
 @app.route('/summarize', methods=['POST'])
 def summarize_url():
     url = request.json.get('url')
     summary = get_page_summary(url)
+    if summary.startswith("Error"):
+        return jsonify({'error': summary}), 400
     return jsonify({'summary': summary})
-
 
 if __name__ == '__main__':
     app.run(debug=True)
